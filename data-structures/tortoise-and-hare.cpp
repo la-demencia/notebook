5236dc189e890d7a29be574171b8b7dbf35dbe7c
Juan Diego <juandiegp17@gmail.com>
2023-03-12 12:52:50 -0600

    Create tortoise-and-hare.cpp

---
+++ data-structures/tortoise-and-hare.cpp
@@ -0,0 +1,35 @@
+/**
+ * Detect cycle in a linked list. Algorithm returns node that starts the cycle and length
+ * of the cycle.
+ * */
+
+struct Node{
+    int data;
+    Node *next = nullptr;
+};
+
+pair<Node *, int> tortoise_and_hare(Node *head){
+    Node *tortoise = head, *hare = head;
+    bool loop = false;
+    while(tortoise && hare && hare->next){
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+        if(tortoise == hare){
+            loop = true;
+            break;
+        }
+    }
+    if(!loop) return {nullptr, 0}; // there is no loop
+    tortoise = head;
+    while(hare != tortoise){
+        hare = hare->next;
+        tortoise = tortoise->next;
+    }
+    int len = 1;
+    hare = tortoise->next;
+    while(hare != tortoise){
+        hare = hare->next;
+        len ++;
+    }
+    return {tortoise, len};
+}
