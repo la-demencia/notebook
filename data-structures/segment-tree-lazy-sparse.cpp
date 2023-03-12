b341c8b3d03aa8e97acf81dd2afd5b559bae5a59
Juan Diego <juandiegp17@gmail.com>
2023-03-12 12:40:01 -0600

    Create segment-tree-lazy-sparse.cpp

---
+++ data-structures/segment-tree-lazy-sparse.cpp
@@ -0,0 +1,47 @@
+template<class T> struct Node{
+    Node *left = nullptr, *right = nullptr;
+    T st = 0, lazy = 0;
+    bool tag = false;
+    int ss, se;
+
+    Node(int ss, int se): ss(ss), se(se){}
+
+    int mid(){ return (se + ss) / 2; }
+    T NEUTRAL(){ return 0; }
+    
+    void push_down(){
+        if(left == nullptr && right == nullptr){
+            left = new Node<T>(ss, mid());
+            right = new Node<T>(mid() + 1, se);
+        }
+        if(tag){
+            left->st = lazy * (mid() - ss + 1);
+            right->st = lazy * (se - mid());
+            left->lazy = right->lazy = lazy;
+            left->tag = right->tag = 1;
+            tag = 0;
+        }
+    }
+ 
+    void update(int qs, int qe, T val){
+        if(ss > qe || se < qs) return;
+        if(qs <= ss && se <= qe){
+            st = val * (se - ss + 1);
+            lazy = val;
+            tag = 1;
+            return;
+        }
+        push_down();
+        left->update(qs, qe, val);
+        right->update(qs, qe, val);
+        st = left->st + right->st;
+    }
+ 
+    T get(int qs, int qe){
+        if(qs <= ss && se <= qe) return st;
+        if(ss > qe || se < qs) return NEUTRAL();
+        push_down();
+        return left->get(qs, qe) + right->get(qs, qe);
+    }
+ 
+};
