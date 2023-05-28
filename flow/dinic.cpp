9ef2f2c564af1eb926d18f2e8acbfc75ae400132
Jherson Medina <38172045+JhersonMedina@users.noreply.github.com>
2023-05-28 11:16:09 -0500

    Update dinic.cpp

--- flow/dinic.cpp
+++ flow/dinic.cpp
@@ -9,57 +9,56 @@
 *
 */
 //Complexity O (n * n * m); where m = number of edges
-template <class T> struct Dinic {
+template <class T> struct  Dinic {
 	struct Edge {
 		int v, inv; T cap, flow;
-		Edge(int v, int inv, T cap, T flow) : v(v), inv(inv), cap(cap), flow(flow) {}
+		Edge (int v, int inv, T cap, T flow) : v(v), inv(inv), cap(cap), flow(flow) {}
 	};
-	int n, s, t;
-	vector <int> level;
-	vector < vector <Edge> > graph;
-	Dinic(int n) : n(n), level(n), graph(n) {}
+	int n, s, t, top;
+	vector <int> level, q;
+	vector <vector <Edge>> graph;
+	Dinic (int n) : n(n), top(0), level(n), q(n + 1), graph(n) {}
 	void addedge(int u, int v, T cap) {
 		graph[u].push_back(Edge(v, graph[v].size(), cap, 0));
 		graph[v].push_back(Edge(u, graph[u].size() - 1, 0, cap));
 	}
 	bool bfs() {
 		fill(level.begin(), level.end(), -1);
-		queue <int> q;
-		q.push(s);
+		q[++top] = s;
 		level[s] = 0;
-		while (!q.empty()) {
-			int u = q.front(); q.pop();
-			for(int i = 0; i < graph[u].size(); ++i) {
-				Edge &e = graph[u][i];
-				if(e.cap > 0 && level[e.v] == -1) {
-					level[e.v] = level[u] + 1;
-					q.push(e.v);
+		while (top) {
+			int u = q[top]; top--;
+			for (int i= 0; i < (int)graph[u].size(); i++) {
+				Edge& e = graph[u][i];
+				if (e.cap > 0 && level[e.v] == -1) {
+					level[e.v]= level[u] + 1;
+					q[++top] = e.v;
 				}
 			}
 		}
 		return level[t] != -1;
 	}
 	T dfs(int u, T pushed) {
-		if(u == t) return pushed;
+		if (u == t) return pushed;
 		T ans = 0;
-		for(int i = 0; i < graph[u].size(); ++i) {
-			Edge &e = graph[u][i];
-			if(e.cap > 0 && level[e.v] == level[u] + 1) {
+		for (int i = 0; i < (int)graph[u].size(); i++) {
+			Edge& e = graph[u][i];
+			if (e.cap >  0 && level[e.v] == level[u] + 1) {
 				T newflow = dfs(e.v, min(pushed, e.cap));
-				ans += newflow; 
-				pushed -= newflow; 
+				ans += newflow;
+				pushed -= newflow;
 				e.cap -= newflow, e.flow += newflow;
 				graph[e.v][e.inv].cap += newflow;
 				graph[e.v][e.inv].flow -= newflow;
-				if(pushed == 0) return ans;
+				if (pushed == 0) return ans;
 			}
 		}
-		if(!ans) level[u] = -1;
+		if (!ans) level[u] = -1;
 		return ans;
 	}
 	T maxflow(int u, int v) {
 		T ans = 0; s = u, t = v;
-		while(bfs()) ans += dfs(s, oo);
+		while (bfs()) ans += dfs(s, oo);
 		return ans;
 	}
 };
